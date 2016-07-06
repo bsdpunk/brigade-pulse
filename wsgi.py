c24edf41abe52fc2bc03a0856434c483dbcdfa98
Zach McCormick <zach@trailblazingtech.com>
2016-07-05 19:19:06 -0500

    fixing import order for whitenoise

--- wsgi.py
+++ wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+from whitenoise.django import DjangoWhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
