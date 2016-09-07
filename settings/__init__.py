5b1644f05989cc9dff429ea3c3bcbe183996bb24
dusty <bsdpunk@gmail.com>
2016-09-07 15:22:33 -0500

    changed wsgi

--- settings/__init__.py
+++ settings/__init__.py
@@ -4,7 +4,7 @@ import dj_database_url
 import raven
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
+print(BASE_DIR)
 SECRET_KEY = os.getenv('SECRET_KEY', '6s=km)xm-kt-xo#l-a+ei4am016_=u=$@90jsxebkve8l49la0')
 
 DEBUG = os.getenv('DJANGO_DEBUG', 'True') == 'True'
@@ -40,14 +40,14 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = 'urls'
-WSGI_APPLICATION = 'settings.wsgi_settings.application'
-
+#WSGI_APPLICATION = 'django.core.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 # We'll use Postgres for both local and production!
 
 DATABASES = {
     'default': dj_database_url.config(conn_max_age=600,
-                                      default='postgres://postgres:postgres@localhost:5432/brigade-pulse')
+        default='postgres://postgres:pass@localhost:5432/brigadepulse')
 }
 
 
@@ -59,11 +59,11 @@ USE_TZ = False
 # TEMPLATE_DIRS allows us to serve index.html at the root + WHITENOISE_ROOT allows us to serve files
 # from the public directory directly.  Probably not *best* practice, but it works for us.
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticroot')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'public'),)
 WHITENOISE_ROOT = os.path.join(BASE_DIR, 'public')
-
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'app/static'),)
 GITHUB_TOKEN = os.getenv('GITHUB_TOKEN', None)
 MEETUP_API_KEY = os.getenv('MEETUP_API_KEY', None)
 
